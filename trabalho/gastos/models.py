c32f26987903725cb08cd1f505322103d44b690d
Gabriel Boscoli <gabriel_boscoli@hotmail.com>
2020-11-20 20:00:06 -0300

    criando o modelo gastos

--- trabalho/gastos/models.py
+++ trabalho/gastos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Gasto(models.Model):
+    descricao = models.CharField(max_length=100, help_text="Entre com a descrição do gasto")
+    custo = models.DecimalField(help_text="Entre com o custo", decimal_places=2, max_digits=8)
+    data = models.DateField(help_text="Data do gasto no formato DD/MM/AAAA", verbose_name="Data do gasto")
+    
+    def __str__(self):
+        return "descrição: " + self.descricao + "\ncusto: " + self.custo + "\ndata: " + self.data
--- trabalho/trabalho/urls.py
+++ trabalho/trabalho/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from trabalho import views
+#from trabalho import views
 from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gastos/', include('gastos.urls')),
+    #path('gastos/', include('gastos.urls')),
 ]
