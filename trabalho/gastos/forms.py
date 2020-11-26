26d543079b5b8a7cd9011f692fc5cf484bae535d
Gabriel Boscoli <gabriel_boscoli@hotmail.com>
2020-11-26 01:13:22 -0300

    consertando os form models para exibir o help text da data e impedir que o custo seja menor que 0

--- trabalho/gastos/forms.py
+++ trabalho/gastos/forms.py
@@ -9,7 +9,8 @@ from django import forms
 from gastos.models import Gasto
 
 class GastoModel2Form(forms.ModelForm):
-    data = forms.DateField(input_formats=['%d/%m/%Y'], label='Data do gasto')
+    data = forms.DateField(input_formats=['%d/%m/%Y'], label='Data do gasto', help_text="Data do gasto no formato <em>DD/MM/AAAA<em>")
+    custo = forms.DecimalField(help_text="Entre com o custo", min_value=0.0, max_value=9999999999)
     class Meta:
         # usa o model de Gasto
         model = Gasto
--- trabalho/gastos/models.py
+++ trabalho/gastos/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Gasto(models.Model):
     descricao = models.CharField(max_length=100, help_text="Entre com a descrição do gasto")
-    custo = models.DecimalField(help_text="Entre com o custo", decimal_places=2, max_digits=8)
-    data = models.DateField(help_text="Data do gasto no formato DD/MM/AAAA", verbose_name="Data do gasto")
+    custo = models.DecimalField(help_text="Entre com o custo", decimal_places=2, max_digits=10)
+    data = models.DateField(help_text="Data do gasto no formato <em>DD/MM/AAAA<em>")
     
     def __str__(self):
         return "descrição: " + self.descricao + "\ncusto: " + self.custo + "\ndata: " + self.data
