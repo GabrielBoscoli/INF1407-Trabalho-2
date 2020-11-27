d16808c0e91d418066a0a33b6a64245a7f4e7460
Gabriel Boscoli <gabriel_boscoli@hotmail.com>
2020-11-27 05:50:54 -0300

    perfil do usuario. nao esta protegido contra usuarios maliciosos

--- trabalho/trabalho/urls.py
+++ trabalho/trabalho/urls.py
@@ -19,6 +19,8 @@ from trabalho import views
 from django.urls.conf import include
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls.base import reverse_lazy
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +32,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page=reverse_lazy('sec-home'),), name='sec-logout'),
     path('accounts/password_change/', PasswordChangeView.as_view(template_name='registro/password_change_form.html', success_url=reverse_lazy('sec-password_change_done'),), name='sec-password_change'),
     path('accounts/password_change_done/', PasswordChangeDoneView.as_view(template_name='registro/password_change_done.html',), name='sec-password_change_done'),
+    path('accounts/terminaRegistro/<int:pk>/', UpdateView.as_view(template_name='registro/user_form.html', success_url=reverse_lazy('sec-home'), model=User, fields=['first_name','last_name','email',],), name='sec-completaDadosUsuario'),
 ]
--- trabalho/trabalho/views.py
+++ trabalho/trabalho/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import UpdateView
+'''
+from django.urls.base import reverse_lazy
+from django.contrib.auth.models import User
+'''
 
 def homeSec(request):
     return render(request, "registro/homeSec.html")
@@ -19,3 +24,18 @@ def registro(request):
 @login_required
 def paginaSecreta(request):
     return render(request, 'registro/paginaSecreta.html')
+
+# evita que usuários maliciosos modifiquem os dados de outro usuário
+class MeuUpdateView(UpdateView):
+    '''
+    template_name='registro/user_form.html'
+    success_url=reverse_lazy('sec-home')
+    model=User
+    fields=['first_name','last_name','email',]
+    '''
+    
+    def get(self, request, pk, *args, **kwargs):
+        if request.user.id == pk:
+            return super().get(request, pk, args, kwargs)
+        else:
+            return redirect('sec-home')
