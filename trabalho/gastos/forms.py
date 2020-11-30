918e367fef58dd2d9183c578eba36e3161ac9c72
Gabriel Boscoli <gabriel_boscoli@hotmail.com>
2020-11-30 11:44:23 -0300

    mensagens de erro no cadastro do gasto

--- trabalho/gastos/forms.py
+++ trabalho/gastos/forms.py
@@ -10,7 +10,7 @@ from gastos.models import Gasto
 
 class GastoModel2Form(forms.ModelForm):
     data = forms.DateField(input_formats=['%d/%m/%Y'], label='Data do gasto', help_text="Data do gasto no formato <em>DD/MM/AAAA<em>")
-    custo = forms.DecimalField(help_text="Entre com o custo", min_value=0.0, max_value=9999999999)
+    custo = forms.DecimalField(help_text="Entre com o custo", min_value=0.0, max_value=9999999999, decimal_places=2, max_digits=10)
     
     class Meta:
         # usa o model de Gasto
